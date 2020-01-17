b0c157f998ca5b98289fb8d0103c9654e52567d6
Vasilii Shabanov <shabspb@gmail.com>
2020-01-17 12:54:09 +0300

    первое задание

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 "use strict";
 
-let arr = ['plum.png', 'orange.png', 'apple.png'];
-console.log(typeof(arr));
+let money = +prompt("Ваш бюджет на месяц?", "");
+let time = prompt("Введите дату в формате YYYY - MM - DD", "YYYY - MM - DD");
 
-console.log("arr" + " - object");
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let mustExpenses = prompt("Введите обязательную статью расходов в этом месяце", "");
+let cost = +prompt("Во сколько обойдется?", "");
+let mustExpenses2 = prompt("Введите обязательную статью расходов в этом месяце", "");
+let cost2 = +prompt("Во сколько обойдется?", "");
+
+let oneDayBudget = (appData.budget - cost - cost2) / 30;
+alert("Ваш бюджет на 1 день - " + oneDayBudget);
+
+
+
+
 
 
