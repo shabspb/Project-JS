b14b38e86b4d50224addcd95f4dc68ef7c89b1ad
Vasilii Shabanov <shabspb@gmail.com>
2020-01-17 14:07:36 +0300

    правильное решение задачи

--- script.js
+++ script.js
@@ -2,24 +2,25 @@
 
 let money = +prompt("Ваш бюджет на месяц?", "");
 let time = prompt("Введите дату в формате YYYY - MM - DD", "YYYY - MM - DD");
-let mustExpenses = prompt("Введите обязательную статью расходов в этом месяце", "");
-let cost = +prompt("Во сколько обойдется?", "");
-let mustExpenses2 = prompt("Введите обязательную статью расходов в этом месяце", "");
-let cost2 = +prompt("Во сколько обойдется?", "");
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses: {
-        mustExpenses : cost,
-        mustExpenses2 : cost2
-    },
+    expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
 };
 
-alert("Ваш бюджет на 1 день = " + (appData.budget - appData.expenses.mustExpenses - appData.expenses.mustExpenses2) / 30 + " руб");
+let mustExpenses = prompt("Введите обязательную статью расходов в этом месяце", "");
+let cost = +prompt("Во сколько обойдется?", "");
+let mustExpenses2 = prompt("Введите обязательную статью расходов в этом месяце", "");
+let cost2 = +prompt("Во сколько обойдется?", "");
+
+appData.expenses[mustExpenses] = cost;
+appData.expenses[mustExpenses2] = cost2;
+
+alert("Ваш бюджет на 1 день = " + appData.budget / 30 + " руб");
 
 
 
