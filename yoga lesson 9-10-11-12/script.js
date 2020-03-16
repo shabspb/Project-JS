118b351dbc2b3aa85648c91f2719998ac1189381
Vasilii Shabanov <shabspb@gmail.com>
2020-03-17 00:24:47 +0300

    decision 13 lesson

--- yoga lesson 9-10-11-12/script.js
+++ yoga lesson 9-10-11-12/script.js
@@ -88,8 +88,8 @@ window.addEventListener('DOMContentLoaded', function () {
 
     let overlay = document.querySelector('.overlay'),
         close = document.querySelector('.popup-close'),
-        more = document.querySelector('.more');
-
+        more = document.querySelector('.more'),
+        descriptionBtn = document.querySelectorAll('.description-btn'); // получаем кнопку узнать подробнее
 
     more.addEventListener('click', function () {
         overlay.style.display = 'block';
@@ -102,6 +102,66 @@ window.addEventListener('DOMContentLoaded', function () {
         more.classList.remove('more-splash');
         document.body.style.overflow = '';
     });
+
+    descriptionBtn.forEach(function (item) {
+        item.addEventListener('click', function () {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    //Form
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо, скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так',
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div'),
+        contactForm = document.getElementById('form');
+
+    statusMessage.classList.add('status');
+
+    function sendRequest(data) {
+        data.addEventListener('submit', function (event) {
+            event.preventDefault();
+            data.appendChild(statusMessage);
+
+            let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+            let formData = new FormData(data);
+
+            let obj = {};
+            formData.forEach(function (value, key) {
+                obj[key] = value;
+            });
+            let json = JSON.stringify(obj);
+
+            request.send(json);
+
+            request.addEventListener('readystatechange', function () {
+                if (request.readyState < 4) {
+                    statusMessage.innerHTML = message.loading;
+                } else if (request.readyState === 4 && request.status == 200) {
+                    statusMessage.innerHTML = message.success;
+                } else {
+                    statusMessage.innerHTML = message.failure;
+                }
+            });
+
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+            }
+        });
+    }
+    sendRequest(form);
+    sendRequest(contactForm);
 });
 
 // Второе задание
